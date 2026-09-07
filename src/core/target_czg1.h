74ae6fea97fa24a4d25ab7ab22bbd63aa876c35d
pcpapc172 <apkme11e@gmail.com>
2026-09-07 15:30:23 +0330

    a16: try nfds=512

--- src/core/target_czg1.h
+++ src/core/target_czg1.h
@@ -270,5 +270,5 @@
 
 /* SLIDE mode pselect shift (pselect vs select stack frame diff 16B = 2 words) */
 #define SLIDE_PSELECT_WORD_SHIFT 1
-#define SLIDE_PSELECT_NFDS 320
+#define SLIDE_PSELECT_NFDS 512
 #define SLIDE_USE_SELECT 1
